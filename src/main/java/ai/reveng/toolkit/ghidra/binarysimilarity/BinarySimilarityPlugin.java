611278fa9ad9d0aaa5116c1560dbf155b685bae6
Florian Magin <fmagin@users.noreply.github.com>
2025-02-14 13:56:07 +0100

    Set plugin status to released (so it auto activates)

--- src/main/java/ai/reveng/toolkit/ghidra/binarysimilarity/BinarySimilarityPlugin.java
+++ src/main/java/ai/reveng/toolkit/ghidra/binarysimilarity/BinarySimilarityPlugin.java
@@ -58,7 +58,7 @@ import java.util.Optional;
  */
 //@formatter:off
 @PluginInfo(
-	status = PluginStatus.STABLE,
+	status = PluginStatus.RELEASED,
 	packageName = ReaiPluginPackage.NAME,
 	category = PluginCategoryNames.COMMON,
 	shortDescription = "Support for Binary Similarity Featrues of RevEng.AI Toolkit.",
