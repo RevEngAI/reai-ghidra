ea99caf84543e0ecf6428a1d0dc92a4ccf83a5a8
Florian Magin <fmagin@users.noreply.github.com>
2025-09-26 16:10:28 +0200

    Add test for Auto Unstrip Dialog

---
+++ src/test/java/ai/reveng/UnstripTest.java
@@ -0,0 +1,166 @@
+package ai.reveng;
+
+import ai.reveng.toolkit.ghidra.core.services.api.AnalysisOptionsBuilder;
+import ai.reveng.toolkit.ghidra.core.services.api.mocks.UnimplementedAPI;
+import ai.reveng.toolkit.ghidra.core.services.api.types.*;
+import ai.reveng.toolkit.ghidra.plugins.BinarySimilarityPlugin;
+import ghidra.program.database.ProgramBuilder;
+import ghidra.program.model.data.Undefined;
+import ghidra.util.task.TaskMonitor;
+import org.junit.Test;
+
+import java.util.Iterator;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class UnstripTest extends RevEngMockableHeadedIntegrationTest{
+
+
+    @Test
+    public void testFinishedUnstrip() throws Exception {
+
+        var tool = env.getTool();
+        var service = addMockedService(tool, new UnimplementedAPI() {
+
+
+            @Override
+            public AutoUnstripResponse autoUnstrip(AnalysisID analysisID) {
+                return new AutoUnstripResponse(
+                        100,
+                        "STATUS",
+                        0,
+                        List.of(new AutoUnstripResponse.Match(new FunctionID(1), 0x1000, "unstripped_function_name") ),
+                        false,
+                        null
+                );
+            }
+
+            @Override
+            public BinaryID analyse(AnalysisOptionsBuilder binHash) {
+                return new BinaryID(1);
+            }
+
+            @Override
+            public AnalysisID getAnalysisIDfromBinaryID(BinaryID binaryID) {
+                return new AnalysisID(binaryID.value());
+            }
+
+            @Override
+            public AnalysisStatus status(AnalysisID analysisID) {
+                return AnalysisStatus.Complete;
+            }
+
+            @Override
+            public List<FunctionInfo> getFunctionInfo(BinaryID binaryID) {
+                return List.of(new FunctionInfo(new FunctionID(1), "default_function_info_name", 0x1000L, 10));
+            }
+        });
+
+        addPlugin(tool, BinarySimilarityPlugin.class);
+        var builder = new ProgramBuilder("mock", ProgramBuilder._X86, this);
+        var func = builder.createEmptyFunction(null, "0x1000", 10, Undefined.getUndefinedDataType(4));
+
+        var programWithID = service.analyse(builder.getProgram(), null, TaskMonitor.DUMMY);
+
+        env.showTool(programWithID.program());
+        // Get the auto unstrip action and execute it
+        var action = getAction(tool, "Auto Unstrip");
+        performAction(action, false);
+
+        // Wait for the dialog to appear and interact with it
+        var dialog = waitForDialogComponent("Auto Unstrip");
+        waitForSwing();
+
+        assertEquals("unstripped_function_name", func.getName());
+        assertEquals("RevEng.AI", func.getParentNamespace().getName(true));
+
+    }
+
+    @Test
+    public void testProgressingUnstrip() throws Exception {
+        var tool = env.getTool();
+        Iterator<AutoUnstripResponse> responses = List.of(
+                new AutoUnstripResponse(
+                        0,
+                        "QUEUED",
+                        0,
+                        List.of(),
+                        false,
+                        null
+                ),
+// The poll interval is not configurable, so we can only test two responses before hitting a Ghidra test timeout
+//                new AutoUnstripResponse(
+//                        50,
+//                        "PROCESSING",
+//                        0,
+//                        List.of(new AutoUnstripResponse.Match(new FunctionID(1), 0x1000, "unstripped_function_name") ),
+//                        false,
+//                        null
+//                ),
+                new AutoUnstripResponse(
+                        100,
+                        "COMPLETED",
+                        0,
+                        List.of(new AutoUnstripResponse.Match(new FunctionID(1), 0x1000, "unstripped_function_name") ),
+                        false,
+                        null
+                )
+        ).iterator();
+
+        var service = addMockedService(tool, new UnimplementedAPI() {
+
+
+            @Override
+            public AutoUnstripResponse autoUnstrip(AnalysisID analysisID) {
+                return responses.next();
+            }
+
+            @Override
+            public BinaryID analyse(AnalysisOptionsBuilder binHash) {
+                return new BinaryID(1);
+            }
+
+            @Override
+            public AnalysisID getAnalysisIDfromBinaryID(BinaryID binaryID) {
+                return new AnalysisID(binaryID.value());
+            }
+
+            @Override
+            public AnalysisStatus status(AnalysisID analysisID) {
+                return AnalysisStatus.Complete;
+            }
+
+            @Override
+            public List<FunctionInfo> getFunctionInfo(BinaryID binaryID) {
+                // the function info will return a name, but it will _not_ be the unstripped name
+                return List.of(new FunctionInfo(new FunctionID(1), "default_function_info_name", 0x1000L, 10));
+            }
+        });
+
+        addPlugin(tool, BinarySimilarityPlugin.class);
+        var builder = new ProgramBuilder("mock", ProgramBuilder._X86, this);
+        // We provide no function name, so ghidra will assign the default "FUN_1000" name
+        var func = builder.createEmptyFunction(null, "0x1000", 10, Undefined.getUndefinedDataType(4));
+
+        var programWithID = service.analyse(builder.getProgram(), null, TaskMonitor.DUMMY);
+
+        env.showTool(programWithID.program());
+        // Get the auto unstrip action and execute it
+        var action = getAction(tool, "Auto Unstrip");
+        performAction(action, false);
+
+        // Wait for the dialog to appear and interact with it
+        var dialog = waitForDialogComponent("Auto Unstrip");
+        waitForSwing();
+
+        waitForCondition(() -> !responses.hasNext());
+        waitForSwing();
+        // Specifically test that the function has been renamed to the unstripped name
+        assertEquals("unstripped_function_name", func.getName());
+        // And also that it is part of the RevEng.AI namespace
+        assertEquals("RevEng.AI", func.getParentNamespace().getName(true));
+
+    }
+
+}
